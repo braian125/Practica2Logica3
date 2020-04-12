acbb06ef2a31b840e8741c032031a2e3a5b1bcf7
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-04-12 16:25:11 -0500

    fix app errors

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,5 @@
 package app;
 
-import avl.ArbolAVL;
-import avltree.Palabra;
 import narytree.Narytree;
 
 public class App {
@@ -10,13 +8,5 @@ public class App {
         Narytree tree = new Narytree();
         System.out.println("1 " + tree.getRoot().getInfo().getName());
         tree.displayTree(tree.getRoot().getChildren(), tree.getRoot().getInfo());
-
-        /* ArbolAVL arbolAVL = new ArbolAVL();
-        arbolAVL.insertarDato('a');
-        arbolAVL.insertarDato('b');
-
-        System.out.println(arbolAVL.buscar('a')); */
-
-        //System.out.println( "Buscar = " + arbolAVL.buscar("palabra") );
     }
 }
